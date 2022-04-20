d691c6278232b9445dc14fb5551d6549582562c1
James Bowers <jamesbowers123@gmail.com>
2022-04-20 10:33:22 +0100

    pass lowercase tests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { sayHello } = require('./lib/strings');
 const { uppercase } = require('./lib/strings');
+const { lowercase } = require('./lib/strings');
 
 const app = express();
 
@@ -19,5 +20,9 @@ app.get('/strings/hello/:string', (req, res) => {
 app.get('/strings/upper/:string', (req, res) => {
     res.status(200).json({ result: uppercase(req.params.string) });
 });
+
+app.get('/strings/lower/:string', (req, res) => {
+    res.status(200).json({ result: lowercase(req.params.string)});
+})
 
 module.exports = app;
