26aebffcc111b16641267994565f3ff5ee820aa4
James Bowers <jamesbowers123@gmail.com>
2022-04-20 10:29:33 +0100

    pass uppercase string tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { sayHello } = require('./lib/strings');
+const { uppercase } = require('./lib/strings');
 
 const app = express();
 
@@ -11,8 +12,12 @@ app.get('/strings/hello/turtle', (req, res) => {
     res.status(200).json({ result: 'Hello, turtle!' });
 });
 
-app.get('/string/hello/:string', (req, res) => {
+app.get('/strings/hello/:string', (req, res) => {
     res.json( { result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+    res.status(200).json({ result: uppercase(req.params.string) });
 });
 
 module.exports = app;
